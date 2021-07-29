bdafb0adb5ee4d9173bd80730ba187012070030c
sumitsingh159 <79156872+sumitsingh159@users.noreply.github.com>
2021-07-30 02:35:26 +0530

    Add files via upload

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,28 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Courses',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Workshops',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact Us',
+        url: 'https://www.hobit.in/',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Log Out',
+        url: '#',
+        cName: 'nav-links-mobile'
+    }
+
+]
